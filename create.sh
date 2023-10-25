0bed8b897d90f6cfcb11edbf281df15eef891519
kimberlytrj <kimberly.trejo@uabc.edu.mx>
2023-10-24 20:17:11 -0700

    Second commit, structure created

--- create.sh
+++ create.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+rm -rf webapp
+
+mkdir -p webapp/{css,js,docs/{text,python},img}
+
+cd webapp
+
+for file in index.html style.css app.js main.py
+do
+    touch ./"${file}"
+done
+
+curl -o meme.jpg "https://pm1.aminoapps.com/7021/417429318f6b29b6bc737fb95ba1ddafc9ba2decr1-720-642v2_hq.jpg"
+
+read -p "Introduce la cantidad de archivos a crear:" numfiles
+    for x in $( seq 1 $numfiles )
+    do
+        echo "Archivo ${x}" > "file${x}.txt"
+    done
